128064a1c1e8a1b607eaaebd092c259eb5f61b75
tianlong <tianlong@momenta.ai>
2018-09-17 20:27:36 +0800

    add some

--- backup_go.sh
+++ backup_go.sh
@@ -1,16 +1,11 @@
 #!/bin/bash -xe
 
-backup_dir=$(realpath .)
-
 if [[ ! $UID == 0 ]];then
   echo "please run as root"
   exit 1
 fi
 
-if [[ ! -d ${backup_dir} ]];then
-  echo "cannot connect samba"
-  exit 1
-fi
+backup_dir=$(realpath ./data)
 
 id=$(date +"%Y%m%d-%H%M%S")
 proj_dir=$(dirname $0)
@@ -23,6 +18,8 @@ snap_vg=$target_vg
 snap_lv=snap_$id
 snap_dev=/dev/${snap_vg}/${snap_lv}
 snap_mount_dir=/snapshot/${snap_lv}
+
+mkdir -p $backup_dir
 
 if [[ -z $(lvs | grep ${snap_lv}) ]];then
   lvcreate -L 30G -s -n $snap_lv ${target_dev}
